241db2ddff25ad04638d24f68c1799e249d5a8b5
Silviu Iosif <6332838@rts-365.dk>
2020-09-06 16:47:30 +0200

    adding index.html and folders css, img and js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let array = ["../img/greenGarden.jpg", "../img/theBand.jpg", "../img/bear.jpg", "../img/earth.jpg", "../img/starwars.jpg","../img/immune.jpg"];
+
+let backgroundImage = document.querySelectorAll(".gridElement header");
+
+for(i = 0; i < backgroundImage.length; i++){
+    backgroundImage[i].style.backgroundImage = "url("+ array[i] +")";
+}
+
